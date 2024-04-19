a74de56f8a9075e28731ab11d0aae321ba579d6e
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-04-19 14:49:36 -0700

    remove device extensions

--- main.cpp
+++ main.cpp
@@ -43,8 +43,8 @@ const std::vector<const char*> validationLayers = {
 
 const std::vector<const char*> deviceExtensions = {
     VK_KHR_SWAPCHAIN_EXTENSION_NAME,
-    "VK_KHR_portability_subset",
-    "VK_KHR_get_physical_device_properties2",
+    //"VK_KHR_portability_subset",
+    //"VK_KHR_get_physical_device_properties2",
     VK_KHR_SURFACE_EXTENSION_NAME,
     //VK_EXT_METAL_SURFACE_EXTENSION_NAME,
     //VK_MVK_MACOS_SURFACE_EXTENSION_NAME,
